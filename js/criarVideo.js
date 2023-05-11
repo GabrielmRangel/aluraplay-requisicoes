fbd3af1d0b30bcb0a147c83e818809f4d23122a2
Gabriel Mendes Rangel <gabrielmendesrangel2@gmail.com>
2023-05-11 19:14:25 -0300

    Projeto finalizado.

--- js/criarVideo.js
+++ js/criarVideo.js
@@ -10,8 +10,15 @@ async function criarVideo(evento){
     const titulo = document.querySelector('[data-titulo]').value;
     const descricao = Math.ceil(Math.random() * 10).toString();
 
-    await conectaApi.criaVideo(titulo, descricao, url, imagem);
-    window.location.href = "../pages/envio-concluido.html";
+    try{
+        await conectaApi.criaVideo(titulo, descricao, url, imagem);
+        window.location.href = "../pages/envio-concluido.html";
+    } catch (e) {
+        alert(e);
+        formulario.innerHTML = `<img src="../img/error404.jpg" class="img-error">
+        <h2 class="mensagem__titulo">Não foi possível carregar o vídeo.</h2>`
+        document.body.style.background = "white";
+    }
 }
 
 formulario.addEventListener('submit', evento => criarVideo(evento));
